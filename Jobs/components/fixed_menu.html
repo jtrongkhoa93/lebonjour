8ae695e106d7188e11d0aa65f1d59edd56fb20af
jtrongkhoa93 <43825182+jtrongkhoa93@users.noreply.github.com>
2022-06-23 22:46:16 +0700

    Delete Jobs directory

--- Jobs/components/fixed_menu.html
+++
@@ -1,5 +0,0 @@
-
-<body>
-        <a href="#home" title="Go to top" id="up"><i class="fas fa-arrow-up"></i></a>
-</body>
-</html>
--- Jobs/php/get.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo 'Dữ Liệu Chúng Tôi Nhận Được Là <br/>';
-if (isset($_GET['id'])){
-foreach ($_GET as $key => $val)
-{
-    echo '<strong>' . $key . ' => ' . $val . '</strong><br/>';
-}
-}
-?>
